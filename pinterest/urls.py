ebed22bbdafc6c970f634d906ce9d5adebce549c
sajad <zareisajad@protonmail.com>
2022-02-01 11:23:47 +0330

    search query

--- pinterest/urls.py
+++ pinterest/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import home, search
 
 app_name = 'pinterest'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('search/', search, name='search'),
 ]
--- pinterest/views.py
+++ pinterest/views.py
@@ -9,3 +9,13 @@ def home(request):
     pins = Pin.objects.all()
     context = {'pins':pins[:49]}
     return render(request, 'home.html', context)
+
+
+@login_required
+def search(request):
+    query = request.GET.get('q')
+    pins = Pin.objects.filter(
+        title__icontains=query,
+        description__icontains=query).all()
+    context = {'pins':pins[:49]}
+    return render(request, 'home.html', context)
